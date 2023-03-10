2ac5bf1482f5f2d9897fbdf4b881de4a3996b45f
brwnalima <bruna.lima@edu.unirio.br>
2023-03-10 17:19:25 -0300

    ainda não está perfeito

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// VARIÁVEIS > query só funcionou especificando a tag antes
+
+const display = document.querySelector('div.display')
+const btn = document.querySelector('div.calculator__keys');
+
+// faz com queo btn.textContent vá para o display
+
+btn.addEventListener('click', e => {
+    if (e.target.matches('button.number')) {
+        const btnContent = e.target.textContent
+        let eu = display.textContent
+        display.textContent = btnContent
+
+    }
+})
+
+const somar = (n1, n2) => {
+    return n1 + n2
+}
+
+const subtrair = () => {
+    console.log("subtraindo");
+}
+
+const multiplicar = () => {
+    console.log("multiplicando");
+}
+
+const dividir = () => {
+    console.log("dividindo");
+}
+
+
+
